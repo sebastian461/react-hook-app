118beab509071ba05db1ace5123a69df5fc2ca98
sebastian461 <sebastiano-461@hotmail.com>
2023-12-07 18:05:08 -0500

    useState

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { HooksApp } from "./HooksApp";
+import { CounterApp } from "./01-useState/CounterApp";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    {/* <HooksApp /> */}
+    <CounterApp />
+  </React.StrictMode>
+);
