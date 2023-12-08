df913e68ec2c4378721351c324a2404827fa6af3
sebastian461 <sebastiano-461@hotmail.com>
2023-12-08 14:45:10 -0500

    Custom hook contador

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { HooksApp } from "./HooksApp";
 import { CounterApp } from "./01-useState/CounterApp";
+import { CounterWithCustomHook } from "./01-useState/CounterWithCustomHook";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     {/* <HooksApp /> */}
-    <CounterApp />
+    {/* <CounterApp /> */}
+    <CounterWithCustomHook />
   </React.StrictMode>
 );
