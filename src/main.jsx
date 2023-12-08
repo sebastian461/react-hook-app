79980dd8a572295de26a4492ae80d951f89db24e
sebastian461 <sebastiano-461@hotmail.com>
2023-12-08 16:37:52 -0500

    useEffect (definición)

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { HooksApp } from "./HooksApp";
-import { CounterApp } from "./01-useState/CounterApp";
-import { CounterWithCustomHook } from "./01-useState/CounterWithCustomHook";
+//import { HooksApp } from "./HooksApp";
+//import { CounterApp } from "./01-useState/CounterApp";
+//import { CounterWithCustomHook } from "./01-useState/CounterWithCustomHook";
+import { SimpleForm } from "./02-useEffect/SimpleForm";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     {/* <HooksApp /> */}
     {/* <CounterApp /> */}
-    <CounterWithCustomHook />
+    {/* <CounterWithCustomHook /> */}
+    <SimpleForm />
   </React.StrictMode>
 );
