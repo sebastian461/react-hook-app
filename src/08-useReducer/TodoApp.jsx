5566d5aac58d4e5f47fe35a90e3495a46e836869
sebastian461 <sebastiano-461@hotmail.com>
2023-12-11 18:03:25 -0500

    TodoApp
    Se creo un componente de TODOS con varias técnicas aprendidas hasta el momento

---
+++ src/08-useReducer/TodoApp.jsx
@@ -0,0 +1,37 @@
+import { useEffect } from "react";
+import { TodoList, TodoAdd } from "./index";
+import { useTodo } from "../hooks/useTodo";
+
+export const TodoApp = () => {
+  const {
+    countTodos,
+    handleDeleteTodo,
+    handleNewTodo,
+    handleToggleTodo,
+    pendingTodos,
+    state,
+  } = useTodo();
+
+  return (
+    <>
+      <h1>
+        TodoApp: {countTodos}, <small>pendientes: {pendingTodos}</small>
+      </h1>
+      <hr />
+      <div className="row">
+        <div className="col-7">
+          <TodoList
+            state={state}
+            onDeleteTodo={handleDeleteTodo}
+            onToggleTodo={handleToggleTodo}
+          />
+        </div>
+        <div className="col-5">
+          <h4>Agregar TODO</h4>
+          <hr />
+          <TodoAdd onNewTodo={handleNewTodo} />
+        </div>
+      </div>
+    </>
+  );
+};
