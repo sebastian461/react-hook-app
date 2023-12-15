8748baf464d9aa8573fe47f537188a604b24e120
sebastian461 <sebastiano-461@hotmail.com>
2023-12-15 14:51:57 -0500

    useContext (HomePage)

--- src/09-useContext/HomePage.jsx
+++ src/09-useContext/HomePage.jsx
@@ -1,8 +1,16 @@
+import { useContext } from "react";
+import { UserContext } from "./context/UserContext";
+
 export const HomePage = () => {
+  const { user } = useContext(UserContext);
+
   return (
     <>
-      <h1>HomePage</h1>
+      <h1>
+        HomePage <small aria-label="name">{user?.name}</small>
+      </h1>
       <hr />
+      <pre aria-label="pre">{JSON.stringify(user, null, 3)}</pre>
     </>
   );
 };
